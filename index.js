f10ef51d985b133adf71a6a9b2879c43921f1de4
JoanaDalmeida <celestina.pricilia@gmail.com>
2016-01-20 18:12:44 +0100

    Add welcome page, error and layout views

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.all('/', function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
     res.render('index.jade', {
-      title: 'Home'
+      title: 'Data visualization'
     });
 });
 
