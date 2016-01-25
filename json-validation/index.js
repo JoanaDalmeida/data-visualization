f3c318904c874e52f4f17b5395d79de88912d048
JoanaDalmeida <celestina.pricilia@gmail.com>
2016-01-25 01:44:47 +0100

    Json validation

---
+++ json-validation/index.js
@@ -0,0 +1,60 @@
+var Validator = require('jsonschema').Validator;
+var v = new Validator();
+
+var logger = require('../logger');
+
+//Config file
+config = require('../config');
+
+var sampleJson = '{ "expenses" : [' +
+		'{ "type":"type1" , "value":2, "date":"12/04/2015" },' +
+		'{ "type":"type2" , "value":3, "date":"12/04/2015" },' +
+		'{ "type":"type3" , "value":6, "date":"12/04/2015" } ]}';
+
+function getSampleJson() {
+	return sampleJson;
+}
+
+function isValid(jsonToParse) {
+	var response = {
+		isValid: true,
+		errors: []
+	}
+	if(jsonToParse) {
+		try {
+			var expenses = JSON.parse(jsonToParse).expenses;
+		} catch(err) {
+			response.isValid = false;
+			response.errors.push("Invalid Json Data");
+			return response;
+		}
+		
+		logger.info("expenses ", expenses);
+
+		for(var i=0; i < expenses.length; i++) {
+			var date = new Date(expenses[i].date);
+			if(true === isNaN(date)) {
+				//continue let the jsonschema library handle the error.
+			} else {
+				expenses[i].date = date;
+			}
+			var errors = v.validate(expenses[i], config.expenseSchema).errors;
+			if(0 < errors.length) {
+				response.isValid = false;
+				for(var j = 0; j < errors.length; j++) {
+					response.errors.push("line " + (i+1) + " : " + errors[j]);
+				}
+
+			}
+		}
+	} else {
+		response.isValid = false;
+		response.errors.push("Invalid Json Data");
+	}
+	return response;
+}
+
+module.exports = {
+	getSampleJson: getSampleJson,
+	isValid: isValid
+}
