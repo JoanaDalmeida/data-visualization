658879a0602062c469b9a272d090547e8286d4c1
JoanaDalmeida <celestina.pricilia@gmail.com>
2016-01-25 01:45:46 +0100

    Updates views

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ var express = require('express');
 var cors = require('cors');
 var app = express();
 var path = require('path');
+var bodyParser = require('body-parser');
+
+validator = require('./json-validation');
+
 
 app.use(cors());
 
@@ -13,19 +17,35 @@ config = require('./config');
 //Load logger
 var logger = require('./logger');
 
+
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser());
 
 app.all('/', function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
     res.render('index.jade', {
-      title: 'Data visualization'
+      title: 'Data visualization',
+      jsonValidationErrors: null
     });
 });
 
-app.post('/parseData', function isReal(req, res, next){
-    logger.info('Draw graph');
-    res.json({});
+app.post('/parseData', function parseData(req, res, next){
+    logger.info('JsonData ', req.body);
+	var data = req.body.data;
+	var response = validator.isValid(data);
+	logger.info('response ', response);
+	if(true === response.isValid) {	
+    	res.render('results.jade', {
+	      title: 'Data visualization',
+	      expenses: JSON.parse(data).expenses
+	    });
+	} else {
+		res.render('index.jade', {
+	      title: 'Data visualization',
+	      jsonValidationErrors: response.errors
+	    });
+	}
 });
 
 app.listen(config.port, function(){
--- public/javascripts/event.js
+++ public/javascripts/event.js
@@ -1,3 +1,31 @@
-function parseJson(){
-  console.info('parseJson');
+var sampleJson = '{' + '\n' + 
+'   "expenses" : [' + '\n'+
+'      {'+ '\n' +
+'         "type":"type1",' + '\n' + 
+'         "value":2,'+ '\n' + 
+'         "date":"12/04/2013"' + '\n' +
+'      },'+ '\n' + 
+'      {'+ '\n' +
+'         "type":"type2",' + '\n' + 
+'         "value":6,'+ '\n' + 
+'         "date":"12/04/2014"' + '\n' +
+'      },'+ '\n' + 
+'      {'+ '\n' +
+'         "type":"type3",' + '\n' + 
+'         "value":10,'+ '\n' + 
+'         "date":"12/04/2015"' + '\n' +
+'      }'+ '\n' + 
+'   ]' + '\n' + 
+'}';
+
+function addSampleJson() {
+	document.getElementById('json-data').value = sampleJson;
 }
+
+function highlightExpenses(event){
+  	//remove the class from all tr 
+	$('tbody tr').removeClass('highlight');
+	//highlight expenses matching the selected type
+	$('tbody tr[data-type="'+ event.data.type +'"]').addClass('highlight')
+}
+
